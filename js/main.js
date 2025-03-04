1a9152aab7b334d5bc614b9f315cec54b37bfb93
Vany1012 <alvarezvillalvavanya@gmail.com>
2025-03-03 21:49:50 -0600

    Subiendo el ejercicio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+Imprimir los números entre el 5 y el 20, saltando de tres en tres.
+*/
+function deTresEnTres(){
+    for (let vuelta = 5; vuelta<=20; vuelta+=3){
+        console.log(vuelta);
+    };
+};
+deTresEnTres();
